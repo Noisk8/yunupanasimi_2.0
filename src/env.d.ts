e4e4eace7663837ebbfd83290072f5f09768d879
noisk8 <juansk8jaramillo@gmail.com>
2023-07-12 14:56:12 -0500

    de nuevo

--- src/env.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="../.astro/types.d.ts" />
-/// <reference types="astro/client-image" />
-declare module 'hero-patterns'
-declare module 'lunr'
-declare module 'photoswipe-dynamic-caption-plugin'
